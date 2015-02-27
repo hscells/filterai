7c588213c20691e9ff381ff3bd7e66a782432843
Harry Scells <harryscells@gmail.com>
2015-02-26 21:16:04 -0500

    much better loading of file

--- fa.lisp
+++ fa.lisp
@@ -2,12 +2,11 @@
 ;;; Harry Scells 2015
 
 ;; Load the image processing library
-(load "~/quicklisp/setup.lisp")
+(load "quicklisp/setup.lisp")
 (ql:quickload "opticl")
 
 ;; Define packages to use
-(defpackage #:filterai (:use #:cl #:opticl))
-(in-package #:filterai)
+(use-package 'opticl)
 
 ;; Convert an image into RGB pixels
 (defun load-painting (name)
@@ -15,19 +14,16 @@
 
 ;; Do some basic processing on the image (adjust colour values)
 (defun process-painting (img name)
-     (typecase img
-       (8-bit-rgb-image
-        (locally
-            (declare (type 8-bit-rgb-image img))
-          (with-image-bounds (height width)
-              img
+   (typecase img
+      (8-bit-rgb-image
+         (locally (declare (type 8-bit-rgb-image img))
+            (with-image-bounds (height width) img
             (time
-             (loop for i below height
-                do (loop for j below width
-                      do
-                      (multiple-value-bind (r g b)
-                          (pixel img i j)
+               (loop for i below height do
+                  (loop for j below width do
+                     (multiple-value-bind (r g b)
+                        (pixel img i j)
                         (declare (type (unsigned-byte 8) r g b))
                         (setf (pixel img i j)
-                              (values (- 255 r) g b))))))))))
-     (write-png-file name img))
+                           (values (- 255 r) g b))))))))))
+   (write-png-file name img))
